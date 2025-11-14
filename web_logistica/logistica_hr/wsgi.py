d931a633f33e6cb2d8ebce9a7d2b512eec32b425
Jaime OrÃ³stegui <ja.orostegui@duocuc.cl>
2025-11-14 12:59:37 -0300

    fix: corregir inconsistencia en configuraciÃ³n de settings para ASGI/WSGI

--- web_logistica/logistica_hr/wsgi.py
+++ web_logistica/logistica_hr/wsgi.py
@@ -6,7 +6,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logistica_hr.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logistica_hr.settings.development')
 
 application = get_wsgi_application()
 
